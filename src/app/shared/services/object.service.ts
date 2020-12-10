ed428f9a632df43f95b3a29baff9f39dc1e8c25a
Marek Stancik <marek.stancik@cmbprosx-h.local>
2020-12-10 12:02:11 +0100

    making api service more generic

---
+++ src/app/shared/services/object.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from "rxjs";
+import { filter, map } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+import { UserService } from "./user.service";
+import { Message } from "./ws-api/dto";
+import { EntityType } from "./ws-api/dto/events";
+import { WsClient } from "./ws-api/ws-client";
+
+export class ObjectService<T extends {id: string}> {
+
+  public list$: BehaviorSubject<T[]> = new BehaviorSubject(null);
+
+  public get(id: string): Observable<T> {
+    return this.list$.pipe(
+      filter(d => !!d),
+      map(d => d.find(p => p.id === id))
+    );
+  }
+
+  public getAll(): void {
+    this.http.get<T[]>(`${environment.apiUrl}/v1/${this.path}`).subscribe(l => this.list$.next(l));
+  }
+
+  public create(obj: T): Observable<T> {
+    return this.http.post<T>(`${environment.apiUrl}/v1/${this.path}`, obj);
+  }
+
+  public update(obj: T): Observable<T> {
+    return this.http.put<T>(`${environment.apiUrl}/v1/${this.path}/${obj.id}`, obj);
+  }
+
+  public delete(obj: T): Observable<T> {
+    return this.http.delete<T>(`${environment.apiUrl}/v1/${this.path}/${obj.id}`);
+  }
+
+  constructor(private http: HttpClient,user: UserService,entityType: EntityType, private path: string) {
+    const client = new WsClient(http,user);
+    client.listen<T>("update",entityType).subscribe(updatedObj => this.list$.next(this.list$.value.map(obj => obj.id === updatedObj.id ? updatedObj : obj)));
+    client.listen<T>("delete",entityType).subscribe(deletedObj => this.list$.next(this.list$.value.filter(obj => obj.id !== deletedObj.id)));
+    client.listen<T>("create",entityType).subscribe(newObj => this.list$.next([...this.list$.value, newObj]));
+  }
+}
--- src/app/shared/services/route.service.ts
+++ src/app/shared/services/route.service.ts
@@ -1,47 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
-import { filter, map } from "rxjs/operators";
-import { environment } from "src/environments/environment";
 import { Route } from "../models/route.model";
+import { ObjectService } from "./object.service";
 import { UserService } from "./user.service";
-import { WsClient } from "./ws-api/ws-client";
 
 @Injectable({
   providedIn: "root"
 })
-export class RouteService {
+export class RouteService extends ObjectService<Route> {
 
-  public list$: BehaviorSubject<Route[]> = new BehaviorSubject(null);
-
-  public get(id: string): Observable<Route> {
-    return this.list$.pipe(
-      filter(d => !!d),
-      map(d => d.find(p => p.id === id))
-    );
-  }
-
-  public getAll(): void {
-    this.http.get<Route[]>(`${environment.apiUrl}/v1/routes`).subscribe(l => this.list$.next(l));
-  }
-
-  public create(route: Route): Observable<Route> {
-    return this.http.post<Route>(`${environment.apiUrl}/v1/routes`, route);
-  }
-
-  public update(route: Route): Observable<Route> {
-    return this.http.put<Route>(`${environment.apiUrl}/v1/routes/${route.id}`, route);
-  }
-
-  public delete(route: Route): Observable<Route> {
-    return this.http.delete<Route>(`${environment.apiUrl}/v1/routes/${route.id}`);
-  }
-
-  constructor(private http: HttpClient,private user: UserService) {
-    const client = new WsClient(http,user);
-    client.listen<Route>("update","route").subscribe(updatedObj => this.list$.next(this.list$.value.map(obj => obj.id === updatedObj.id ? updatedObj : obj)));
-    client.listen<Route>("delete","route").subscribe(deletedObj => this.list$.next(this.list$.value.filter(obj => obj.id !== deletedObj.id)));
-    client.listen<Route>("create","route").subscribe(newObj => this.list$.next([...this.list$.value, newObj]));
+  constructor(http: HttpClient,user: UserService) {
+    super(http,user,"route","routes");
   }
 }
 
