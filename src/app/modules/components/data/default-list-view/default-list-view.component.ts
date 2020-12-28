0c2b708dbb292323592ff3c42918492cb4aa4dd4
Marek Stancik <marek.stancik@cmbprosx-h.local>
2020-12-28 16:44:58 +0100

    small fixes

--- src/app/modules/components/data/default-list-view/default-list-view.component.ts
+++ src/app/modules/components/data/default-list-view/default-list-view.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-default-list-view",
@@ -6,12 +6,6 @@ import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
   styleUrls: ["./default-list-view.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DefaultListViewComponent implements OnInit {
+export class DefaultListViewComponent {
   public detailOpened = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
